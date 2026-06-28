669ef19f178444dc5a9bee0affb08d13e8f5db5c
Muhammad Usama <muhammadus9139@gmail.com>
2026-06-28 16:29:04 +0500

    Create For in loops.js

---
+++ Arrays in JavaScript/Array Loops/For in loops.js
@@ -0,0 +1,15 @@
+let fruits = ["apple", "banana", "mango"];
+
+for (let index in fruits) {
+    console.log(index, fruits[index]);
+}
+
+let person = {
+    name: "Ali",
+    age: 20,
+    city: "Lahore"
+};
+
+for (let key in person) {
+    console.log(key, person[key]);
+}
