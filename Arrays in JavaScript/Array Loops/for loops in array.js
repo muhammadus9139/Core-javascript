75f44f4b365a73bdbcc6efa4e4cd0eca26bb4b2d
Muhammad Usama <muhammadus9139@gmail.com>
2026-06-28 16:27:54 +0500

    Create for loops in array.js

---
+++ Arrays in JavaScript/Array Loops/for loops in array.js
@@ -0,0 +1,12 @@
+let fruits = ["apple", "banana", "mango", "orange"];
+
+for (let i = 0; i < fruits.length; i++) {
+    console.log(fruits[i]);
+}
+
+let numbers = [10, 20, 30, 40, 50];
+let sum = 0;
+for (let i = 0; i < numbers.length; i++) {
+    sum += numbers[i];
+}
+console.log(sum);
