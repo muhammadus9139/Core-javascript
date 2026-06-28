e3529b2baee214a4e7e9d8df564a83bb2f1362c9
Muhammad Usama <muhammadus9139@gmail.com>
2026-06-28 16:24:49 +0500

    Create Intro to array.js

---
+++ Arrays in JavaScript/Array basics/Intro to array.js
@@ -0,0 +1,10 @@
+let fruits = ["apple", "banana", "mango"];
+let numbers = [1, 2, 3, 4, 5];
+let mixed = [1, "hello", true, null];
+
+console.log(fruits);
+console.log(fruits[0]);
+console.log(fruits[fruits.length - 1]);
+
+fruits[1] = "orange";
+console.log(fruits);
