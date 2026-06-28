724c2cb465c23d032c4360c7b063ce7296b03cff
Muhammad Usama <muhammadus9139@gmail.com>
2026-06-28 16:20:07 +0500

    Create while_loop.js

---
+++ Basics_of_JavaScript/Loops/while_loop.js
@@ -0,0 +1,15 @@
+// While Loop
+let i = 1;
+
+while (i <= 5) {
+    console.log("Number: " + i);
+    i++; // i = i + 1
+}
+
+// Countdown
+let count = 10;
+while (count >= 1) {
+    console.log(count);
+    count--;
+}
+console.log("Blast off! 🚀");
