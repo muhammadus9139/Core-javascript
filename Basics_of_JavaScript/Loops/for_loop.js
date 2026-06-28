55574c30ba245f61fc4e0e05e78edf3045cf6ecd
Muhammad Usama <muhammadus9139@gmail.com>
2026-06-28 16:21:09 +0500

    Create for_loop.js

---
+++ Basics_of_JavaScript/Loops/for_loop.js
@@ -0,0 +1,22 @@
+// For Loop
+// for(initialization; condition; update)
+
+for (let i = 1; i <= 5; i++) {
+    console.log("Number:", i);
+}
+
+// Reverse loop
+for (let i = 10; i >= 1; i--) {
+    console.log(i);
+}
+
+// Step by 2
+for (let i = 0; i <= 20; i += 2) {
+    console.log(i); // 0, 2, 4, 6...
+}
+
+// String ke saath
+let str = "Hello";
+for (let i = 0; i < str.length; i++) {
+    console.log(str[i]);
+}
