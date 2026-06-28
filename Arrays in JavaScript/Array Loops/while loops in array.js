51aaa7aa4886cc22eb1540d2aa089c7893af4559
Muhammad Usama <muhammadus9139@gmail.com>
2026-06-28 16:28:20 +0500

    Create while loops in array.js

---
+++ Arrays in JavaScript/Array Loops/while loops in array.js
@@ -0,0 +1,16 @@
+let fruits = ["apple", "banana", "mango", "orange"];
+
+let i = 0;
+while (i < fruits.length) {
+    console.log(fruits[i]);
+    i++;
+}
+
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let j = 0;
+while (j < numbers.length) {
+    if (numbers[j] % 2 === 0) {
+        console.log(numbers[j]);
+    }
+    j++;
+}
