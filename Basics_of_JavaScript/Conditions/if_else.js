a5e141c22b14ecf1d6a64fd415b8be94047beded
Muhammad Usama <muhammadus9139@gmail.com>
2026-06-28 16:16:10 +0500

    Create if_else.js

---
+++ Basics_of_JavaScript/Conditions/if_else.js
@@ -0,0 +1,29 @@
+// If Else Statement
+let age = 20;
+
+// Simple if
+if (age >= 18) {
+    console.log("Aap adult hain");
+}
+
+// if-else
+if (age >= 18) {
+    console.log("Aap vote de sakte hain");
+} else {
+    console.log("Aap vote nahi de sakte");
+}
+
+// if-else if-else
+let marks = 75;
+
+if (marks >= 90) {
+    console.log("Grade: A+");
+} else if (marks >= 80) {
+    console.log("Grade: A");
+} else if (marks >= 70) {
+    console.log("Grade: B");
+} else if (marks >= 60) {
+    console.log("Grade: C");
+} else {
+    console.log("Grade: Fail");
+}
