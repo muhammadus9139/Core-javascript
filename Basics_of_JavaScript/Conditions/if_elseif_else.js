6389851b5d963e7b5a983e9dc36697b983bae40a
Muhammad Usama <muhammadus9139@gmail.com>
2026-06-28 16:18:15 +0500

    Create if_elseif_else.js

---
+++ Basics_of_JavaScript/Conditions/if_elseif_else.js
@@ -0,0 +1,27 @@
+// If ElseIf Else
+let day = "Monday";
+
+if (day === "Monday") {
+    console.log("Hafta shuru hua");
+} else if (day === "Friday") {
+    console.log("Weekend aa gaya!");
+} else if (day === "Saturday" || day === "Sunday") {
+    console.log("Chhutti hai!");
+} else {
+    console.log("Normal din hai");
+}
+
+// Number example
+let score = 85;
+
+if (score >= 90) {
+    console.log("Excellent!");
+} else if (score >= 80) {
+    console.log("Very Good!");
+} else if (score >= 70) {
+    console.log("Good!");
+} else if (score >= 60) {
+    console.log("Average");
+} else {
+    console.log("Fail");
+}
