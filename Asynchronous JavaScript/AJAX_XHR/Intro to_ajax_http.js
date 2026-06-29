d379334a9771f6f439283c7a783e341434ea95d6
Muhammad Usama <muhammadus9139@gmail.com>
2026-06-29 17:20:21 +0500

    Create Intro to_ajax_http.js

---
+++ Asynchronous JavaScript/AJAX_XHR/Intro to_ajax_http.js
@@ -0,0 +1,27 @@
+// AJAX = Asynchronous JavaScript And XML
+// Page reload ke bina server se data lena
+
+// HTTP Methods:
+// GET    - data lena
+// POST   - data bhejna
+// PUT    - data update karna
+// DELETE - data delete karna
+
+// HTTP Status Codes:
+// 200 - OK
+// 201 - Created
+// 400 - Bad Request
+// 401 - Unauthorized
+// 404 - Not Found
+// 500 - Server Error
+
+// API - Application Programming Interface
+// JSON - JavaScript Object Notation
+// REST API - ek tarika hai server se baat karne ka
+
+let example = {
+    url: "https://jsonplaceholder.typicode.com/users",
+    method: "GET",
+    response: "JSON data"
+};
+console.log(example);
